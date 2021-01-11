161f4991c94ec23587abe73e69dd7dee29227d05
Enigana <zk@seadora.com.ua>
2021-01-11 23:15:48 +0200

    add scroll animation header

--- src/HOC/HeaderDisappearance.js
+++ src/HOC/HeaderDisappearance.js
@@ -1,16 +1,43 @@
-import React from "react";
-import { Animated, View, StyleSheet, SafeAreaView } from "react-native";
+import React, {useRef, useState, useEffect} from "react";
+import {Animated, View, StyleSheet, SafeAreaView, Text, StatusBar} from "react-native";
 import Header from "../components/Header";
 
+const headerHeight = 50;
+
 const HeaderDisappearance = (props) => {
-  return(
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const diffClampTranslate = Animated.diffClamp(scrollY, 0, headerHeight);
+  const translateY = diffClampTranslate.interpolate({
+    inputRange: [0, headerHeight],
+    outputRange: [0, -headerHeight],
+    extrapolate: 'clamp'
+  })
+
+
+  return (
     <SafeAreaView>
       <View>
-        <Animated.View>
-          <Header />
+        <Animated.View style={{
+          transform: [{translateY}],
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          zIndex: 10
+        }}>
+          <Header/>
         </Animated.View>
-
-        <Animated.ScrollView>
+        <Animated.ScrollView bounces={false}
+                             throttle={1}
+                             onScroll={Animated.event([
+                                 {
+                                   nativeEvent: {contentOffset: {y: scrollY}}
+                                 }
+                               ],
+                               {useNativeDriver: true}
+                             )}
+                             contentContainerStyle={{paddingTop: headerHeight}}
+                             refreshControl={false}
+        >
           {props.children}
         </Animated.ScrollView>
       </View>
